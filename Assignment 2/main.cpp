9afe80943dc90b27704fa546226bd84f500f1b4d
ohNuggen <82862725+ohNuggen@users.noreply.github.com>
2021-04-26 13:14:42 +1000

    Create main.cpp

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,26 @@
+#include "tree.hpp"
+
+int main(){
+
+	BST<int> t;
+	
+	t.add_vertex("A", 1);
+	t.add_vertex("B", 3);
+	t.add_vertex("C", 5);
+	t.add_vertex("D", 10);
+	t.add_vertex("G", 20);
+	t.add_vertex("E", 15);
+	t.add_vertex("H", 30);
+	t.add_vertex("F", 17);
+	
+	cout << boolalpha;
+	cout << t.num_vertices() << endl;
+	cout << t.num_edges() << endl;
+
+	cout << t.sum_weight() << endl;
+	
+	for(auto x : t.path_with_largest_weight()){
+		cout << x << " ";
+	}
+	cout << endl;
+}
